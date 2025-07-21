f6dda0b151c3d82fa726a7b3a35bb111b2680085
Alvaro Canovas <hello@acanovas.info>
2025-07-21 14:25:51 +0200

    Update stripe-config.js

--- js/stripe-config.js
+++ js/stripe-config.js
@@ -4,12 +4,12 @@
 window.STRIPE_CONFIG = {
     // Replace these with your actual Stripe price IDs
     priceIds: {
-        '2h': 'price_REPLACE_WITH_2H_PRICE_ID',          // 2h/semana - 65€/mes
-        '2h30': 'price_REPLACE_WITH_2H30_PRICE_ID',      // 2h30/semana - 75€/mes  
-        '3h30': 'price_REPLACE_WITH_3H30_PRICE_ID',      // 3h30/semana - 90€/mes
-        '4h': 'price_REPLACE_WITH_4H_PRICE_ID',          // 4h/semana - 100€/mes
-        '4h30': 'price_REPLACE_WITH_4H30_PRICE_ID',      // 4h30/semana - 110€/mes
-        '6h': 'price_REPLACE_WITH_6H_PRICE_ID',          // 6h/semana - 120€/mes
-        'lab': 'price_REPLACE_WITH_LAB_PRICE_ID'         // LAB Creativo - 50€/mes
+        '2h': 'price_1RnHKLI88Uf0De5ABdbIpbzo',          // 2h/semana - 65€/mes
+        '2h30': 'price_1RnHKjI88Uf0De5AlJg8ohsJ',      // 2h30/semana - 75€/mes  
+        '3h30': 'price_1RnHM3I88Uf0De5AwpFxwhAV',      // 3h30/semana - 90€/mes
+        '4h': 'price_1RnHMLI88Uf0De5APIUkRXLs',          // 4h/semana - 100€/mes
+        '4h30': 'price_1RnHMnI88Uf0De5AVTpmzawd',      // 4h30/semana - 110€/mes
+        '6h': 'price_1RnHN4I88Uf0De5ABqKRZUj8',          // 6h/semana - 120€/mes
+        'lab': 'price_1RnHOCI88Uf0De5AJPnqE9NI'         // LAB Creativo - 50€/mes
     }
 }; 
